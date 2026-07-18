3a1af2ea6650449f5ad339cacdf8328fe913dc96
eleven-arch <washingtons1014@outlook.com>
2026-07-18 19:42:55 -0400

    remove assest

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Benjamin Jasinski",
-  profile: '/profile.png',
+  profile: '',
   designation: "Social Media Content Manager",
   title: "Social Media Content Manager for music, entertainment, and online media brands.",
   description: "I manage and publish digital content across social media platforms, with a focus on music, entertainment, and online media. My responsibilities include content planning, post creation, page management, audience engagement, performance tracking, and coordinating multimedia content. Payments received are for social media content management and related digital media services provided to a U.S.-based company.",
